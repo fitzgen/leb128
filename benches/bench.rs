3262723bd4cc7b2f8abaf84bcdc6a71587fb531b
Nick Fitzgerald <fitzgen@gmail.com>
2018-10-11 23:04:57 -0700

    Use criterion for benchmarking

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,66 +1,75 @@
-#![feature(test)]
-
-extern crate test;
+#[macro_use]
+extern crate criterion;
 extern crate leb128;
 
-#[bench]
-fn write_signed(b: &mut test::Bencher) {
-    let mut buf = [0; 4096];
+use criterion::Criterion;
 
-    b.iter(|| {
-        let mut writable = &mut buf[..];
-        for i in -1025..1025 {
-            test::black_box(leb128::write::signed(&mut writable, i).unwrap());
-        }
+fn write_signed(c: &mut Criterion) {
+    c.bench_function("write signed", |b| {
+        let mut buf = [0; 4096];
+
+        b.iter(|| {
+            let mut writable = &mut buf[..];
+            for i in -1025..1025 {
+                criterion::black_box(leb128::write::signed(&mut writable, i).unwrap());
+            }
+        })
     });
 }
 
-#[bench]
-fn write_unsigned(b: &mut test::Bencher) {
-    let mut buf = [0; 4096];
+fn write_unsigned(c: &mut Criterion) {
+    c.bench_function("write unsigned", |b| {
+        let mut buf = [0; 4096];
 
-    b.iter(|| {
-        let mut writable = &mut buf[..];
-        for i in 0..2050 {
-            test::black_box(leb128::write::unsigned(&mut writable, i).unwrap());
-        }
+        b.iter(|| {
+            let mut writable = &mut buf[..];
+            for i in 0..2050 {
+                criterion::black_box(leb128::write::unsigned(&mut writable, i).unwrap());
+            }
+        })
     });
 }
 
-#[bench]
-fn read_signed(b: &mut test::Bencher) {
-    let mut buf = [0; 4096];
+fn read_signed(c: &mut Criterion) {
+    c.bench_function("read signed", |b| {
+        let mut buf = [0; 4096];
 
-    {
-        let mut writable = &mut buf[..];
-        for i in -1025..1025 {
-            leb128::write::signed(&mut writable, i).unwrap();
+        {
+            let mut writable = &mut buf[..];
+            for i in -1025..1025 {
+                leb128::write::signed(&mut writable, i).unwrap();
+            }
         }
-    }
 
-    b.iter(|| {
-        let mut readable = &buf[..];
-        for _ in -1025..1025 {
-            test::black_box(leb128::read::signed(&mut readable).unwrap());
-        }
+        b.iter(|| {
+            let mut readable = &buf[..];
+            for _ in -1025..1025 {
+                criterion::black_box(leb128::read::signed(&mut readable).unwrap());
+            }
+        })
     });
 }
 
-#[bench]
-fn read_unsigned(b: &mut test::Bencher) {
-    let mut buf = [0; 4096];
+fn read_unsigned(c: &mut Criterion) {
+    c.bench_function("read unsigned", |b| {
+        let mut buf = [0; 4096];
 
-    {
-        let mut writable = &mut buf[..];
-        for i in 0..2050 {
-            leb128::write::unsigned(&mut writable, i).unwrap();
+        {
+            let mut writable = &mut buf[..];
+            for i in 0..2050 {
+                leb128::write::unsigned(&mut writable, i).unwrap();
+            }
         }
-    }
 
-    b.iter(|| {
-        let mut readable = &buf[..];
-        for _ in 0..2050 {
-            test::black_box(leb128::read::unsigned(&mut readable).unwrap());
-        }
+        b.iter(|| {
+            let mut readable = &buf[..];
+            for _ in 0..2050 {
+                criterion::black_box(leb128::read::unsigned(&mut readable).unwrap());
+            }
+        })
     });
 }
+
+criterion_group!(read, read_signed, read_unsigned);
+criterion_group!(write, write_signed, write_unsigned);
+criterion_main!(read, write);
